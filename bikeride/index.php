f878d10b5eab18e35f6106bf92096fed274fb817
vianey <vianeylinares@gmail.com>
2023-08-17 17:27:26 -0700

    Added basic HTML structure.

--- bikeride/index.php
+++ bikeride/index.php
@@ -12,3 +12,15 @@
  * @package Bikeride
  */
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Website title</title>
+    </head>
+    <body>
+
+    </body>
+</html>
