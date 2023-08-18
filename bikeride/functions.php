566f9d895f1e5d1bb572ed9203b056eebb5bdcf2
vianey <vianeylinares@gmail.com>
2023-08-17 18:22:42 -0700

    Enqueued styles.css.

--- bikeride/functions.php
+++ bikeride/functions.php
@@ -7,3 +7,11 @@
  *
  * @package Bikeride
  */
+
+
+function bikeride_scripts(){
+
+    wp_enqueue_style( 'bikeride-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+
+}
+add_action( 'wp_enqueue_scripts', 'bikeride_scripts' );
