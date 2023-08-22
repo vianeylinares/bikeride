47369fe8fc6046020cd1f65ff958202f28e316e3
vianey <vianeylinares@gmail.com>
2023-08-22 16:44:26 -0700

    Added JS functions and variables for mobile menu functionality.

--- bikeride/custom.js
+++ bikeride/custom.js
@@ -1,1 +1,23 @@
 /* Custom JS */
+
+var menuButton = document.getElementById( 'menu-toggle' );
+let menuActive = false;
+
+menuButton.addEventListener( 'click', function handleClick(){
+
+    navMenu = document.getElementsByClassName( 'nav-menu' );
+    console.log(navMenu);
+
+    switch( menuActive ){
+        case true:
+            navMenu[0].style.display = 'none';
+            menuActive = false;
+            break;
+        case false:
+            navMenu[0].style.display = 'flex';
+            menuActive = true;
+            break;
+
+    }
+
+} );
