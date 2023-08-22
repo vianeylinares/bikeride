3ae4ec5effa975203ce5b241cb920324ce145bc6
vianey <vianeylinares@gmail.com>
2023-08-22 16:48:32 -0700

    Added JS function for window resizing monitoring. This will reset main menu activation for desktop and mobile responsive display.

--- bikeride/custom.js
+++ bikeride/custom.js
@@ -21,3 +21,22 @@ menuButton.addEventListener( 'click', function handleClick(){
     }
 
 } );
+
+window.addEventListener( 'resize', function monitorWidth(){
+
+    navMenu = document.getElementsByClassName( 'nav-menu' );
+    navMenu_display = navMenu[0].style.display;
+
+    window_size = window.innerWidth;
+    console.log(window_size);
+
+    if( navMenu_display == 'none' && window_size >= 1200 ){
+        navMenu[0].style.display = 'flex';
+    }
+
+    if( navMenu_display == 'flex' && window_size < 1200 ){
+        navMenu[0].style.display = 'none';
+        menuActive = false;
+    }
+
+} );
