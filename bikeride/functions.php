c8c900240788c62575dc3d67f4b3f2cb7119b84e
vianey <vianeylinares@gmail.com>
2023-08-22 16:36:53 -0700

    Enqueued custom.js file.

--- bikeride/functions.php
+++ bikeride/functions.php
@@ -13,5 +13,7 @@ function bikeride_scripts(){
 
     wp_enqueue_style( 'bikeride-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
 
+    wp_enqueue_script( 'bikeride-scripts', get_template_directory_uri() . '/custom.js' , array(), '', true  );
+
 }
 add_action( 'wp_enqueue_scripts', 'bikeride_scripts' );
