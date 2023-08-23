92583fc89f1367fde31b7f6fba182b3066465e2b
vianey <vianeylinares@gmail.com>
2023-08-23 14:57:24 -0700

    Added WP loop to display blog posts.

--- bikeride/index.php
+++ bikeride/index.php
@@ -14,6 +14,25 @@
 ?>
 <?php get_header(); ?>
 
-Main content area...
+<?php
+
+    // If there are any posts
+    if( have_posts() ):
+        // While posts loop
+        while( have_posts() ): the_post();
+            ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <div><?php the_content(); ?></div>
+            </article>
+            <?php
+        endwhile;
+    else:
+        ?>
+        <p>Nothing to display</p>
+        <?php
+    endif;
+
+?>
 
 <?php get_footer(); ?>
