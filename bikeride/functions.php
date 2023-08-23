a7754a37edf1a8f2010ff4cb2538726ac0fb6e67
vianey <vianeylinares@gmail.com>
2023-08-23 12:36:18 -0700

    Enqueued Google Fonts 'Nanum Gothic' and 'Pacifico'. Also, assigned 'Nanum Gothic' as common font.

--- bikeride/functions.php
+++ bikeride/functions.php
@@ -11,8 +11,9 @@
 
 function bikeride_scripts(){
 
-    wp_enqueue_style( 'bikeride-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
     wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css', 'all' );
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Pacifico&display=swap', array(), null );
+    wp_enqueue_style( 'bikeride-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
 
     wp_enqueue_script( 'bikeride-scripts', get_template_directory_uri() . '/custom.js' , array(), '', true  );
 
