762c9e8a2c20827ddcd331e764955ee4cb88caa7
vianey <vianeylinares@gmail.com>
2023-08-23 15:23:08 -0700

    Added missing link to post title.

--- bikeride/index.php
+++ bikeride/index.php
@@ -22,7 +22,7 @@
         while( have_posts() ): the_post();
             ?>
             <article>
-                <h2><?php the_title(); ?></h2>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                 <div><?php the_content(); ?></div>
             </article>
             <?php
