07c73c03740178ad5c76edb8eb912ff08098ea3e
vianey <vianeylinares@gmail.com>
2023-08-23 14:57:57 -0700

    Created page.php template.

---
+++ bikeride/page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The Page template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Bikeride
+ */
+?>
+<?php get_header(); ?>
+
+    Page main content area...
+
+<?php get_footer(); ?>
