8b757a30516ec1a76175c1c52d5286f4011d4342
vianey <vianeylinares@gmail.com>
2023-08-25 18:06:48 -0700

    At WP customizer added Copyright and Social Network Settings Sections. Also, at footer section in fronted added functionality to display the Copyright and Social Network settings.

--- bikeride/footer.php
+++ bikeride/footer.php
@@ -10,13 +10,13 @@
             </main>
             <footer class="footer">
                 <div class="social">
-                    <a href="#" target="_blank"><i class="fa fa-facebook-square"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-pinterest-square"></i></a>
-                    <a href="#" target="_blank"><i class="fa fa-youtube-square"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_facebook' ) ); ?>" target="_blank"><i class="fa fa-facebook-square"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_instagram' ) ); ?>" target="_blank"><i class="fa fa-instagram"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_pinterest' ) ); ?>" target="_blank"><i class="fa fa-pinterest-square"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_youtube' ) ); ?>" target="_blank"><i class="fa fa-youtube-square"></i></a>
                 </div>
                 <div class="copyright-policies">
-                    &copy; - Copyright 2023
+                <?php echo esc_html( get_theme_mod( 'set_copyright', __( 'Copyright - All Rights Reserved', 'bikeride' ) ) ); ?>
                 </div>
             </footer>
         </div>
--- bikeride/inc/customizer.php
+++ bikeride/inc/customizer.php
@@ -6,3 +6,116 @@
  */
 
 
+function bikeride_customizer( $wp_customize ){
+
+	// Copyright section
+
+	$wp_customize->add_section(
+		'sec_copyright', array(
+			'title'         => __( 'Copyright Settings', 'bikeride' ),
+			'description'   => __( 'Copyright Section', 'bikeride' )
+		)
+	);
+
+			// Copyright
+			$wp_customize->add_setting(
+				'set_copyright', array(
+					'type'                  => 'theme_mod',
+					'default'               => '',
+					'sanitize_callback'     => 'sanitize_text_field'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_copyright', array(
+					'label'         => __( 'Copyright', 'bikeride' ),
+					'description'   => __( 'Please, add your copyright information here', 'bikeride' ),
+					'section'       => 'sec_copyright',
+					'type'          => 'text'
+				)
+			);
+
+
+    // Social networks section
+
+	$wp_customize->add_section(
+		'sec_socials', array(
+			'title'         => __( 'Social Network Settings', 'bikeride' ),
+			'description'   => __( 'Social Network Section', 'bikeride' )
+		)
+	);
+
+			// Facebook
+			$wp_customize->add_setting(
+				'set_facebook', array(
+					'type'                  => 'theme_mod',
+					'default'               => '',
+					'sanitize_callback'     => 'sanitize_url'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_facebook', array(
+					'label'         => __( 'Facebook', 'bikeride' ),
+					'description'   => __( 'Please, add your Facebook link here', 'bikeride' ),
+					'section'       => 'sec_socials',
+					'type'          => 'url'
+				)
+			);
+
+            // Instagram
+			$wp_customize->add_setting(
+				'set_instagram', array(
+					'type'                  => 'theme_mod',
+					'default'               => '',
+					'sanitize_callback'     => 'sanitize_url'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_instagram', array(
+					'label'         => __( 'Instagram', 'bikeride' ),
+					'description'   => __( 'Please, add your Instagram link here', 'bikeride' ),
+					'section'       => 'sec_socials',
+					'type'          => 'url'
+				)
+			);
+
+            // Pinterest
+			$wp_customize->add_setting(
+				'set_pinterest', array(
+					'type'                  => 'theme_mod',
+					'default'               => '',
+					'sanitize_callback'     => 'sanitize_url'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_pinterest', array(
+					'label'         => __( 'Pinterest', 'bikeride' ),
+					'description'   => __( 'Please, add your Pinterest link here', 'bikeride' ),
+					'section'       => 'sec_socials',
+					'type'          => 'url'
+				)
+			);
+
+            // YouTube
+			$wp_customize->add_setting(
+				'set_youtube', array(
+					'type'                  => 'theme_mod',
+					'default'               => '',
+					'sanitize_callback'     => 'sanitize_url'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_youtube', array(
+					'label'         => __( 'YouTube', 'bikeride' ),
+					'description'   => __( 'Please, add your YouTube link here', 'bikeride' ),
+					'section'       => 'sec_socials',
+					'type'          => 'url'
+				)
+			);
+
+}
+add_action( 'customize_register', 'bikeride_customizer' );
