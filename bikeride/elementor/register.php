e78aa5b03f42bef734f63659430e251113d888ad
vianey <vianeylinares@gmail.com>
2023-09-01 12:19:36 -0700

    Registered custom Elementor BR info widget. Also, added related styles for frontend display.

--- bikeride/elementor/register.php
+++ bikeride/elementor/register.php
@@ -5,10 +5,12 @@ function bikeride_register_widgets( $widgets_manager ) {
     require_once get_template_directory() . '/elementor/widgets/heading.php';
     require_once get_template_directory() . '/elementor/widgets/news.php';
     require_once get_template_directory() . '/elementor/widgets/know.php';
+    require_once get_template_directory() . '/elementor/widgets/info.php';
 
     $widgets_manager->register( new \Bikeride_Headging_Widget() );
     $widgets_manager->register( new \Bikeride_News_Widget() );
     $widgets_manager->register( new \Bikeride_Know_Widget() );
+    $widgets_manager->register( new \Bikeride_Info_Widget() );
 
 }
 add_action( 'elementor/widgets/register', 'bikeride_register_widgets' );
---
+++ bikeride/elementor/widgets/info.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Bikeride Info Elementor Widget.
+ *
+ * Elementor widget that inserts info entries.
+ *
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ){
+	exit; // Exit if accessed directly.
+}
+
+
+class Bikeride_Info_Widget extends \Elementor\Widget_Base{
+
+    /**
+     * Get widget name.
+     *
+     * Retrieve widget name.
+     *
+     * @since 1.0.0
+     * @access public
+     * @return string Widget name.
+     */
+    public function get_name(){
+        return 'Bikeride info';
+    }
+
+
+    /**
+     * Get widget title.
+     *
+     * Retrieve widget title.
+     *
+     * @since 1.0.0
+     * @access public
+     * @return string Widget title.
+     */
+    public function get_title(){
+        return esc_html__( 'BR Info', 'bikeride' );
+    }
+
+
+    /**
+     * Get widget icon.
+     *
+     * Retrieve widget icon.
+     *
+     * @since 1.0.0
+     * @access public
+     * @return string Widget icon.
+     */
+    public function get_icon(){
+        return 'eicon-editor-list-ul';
+    }
+
+
+    /**
+     * Get custom help URL.
+     *
+     * Retrieve a URL where the user can get more information about the widget.
+     *
+     * @since 1.0.0
+     * @access public
+     * @return string Widget help URL.
+     */
+    public function get_custom_help_url(){
+        return 'https://essentialwebapps.com/category/elementor-tutorial/';
+    }
+
+
+    /**
+     * Get widget categories.
+     *
+     * Retrieve the list of categories the widget belongs to.
+     *
+     * @since 1.0.0
+     * @access public
+     * @return array Widget categories.
+     */
+    public function get_categories(){
+        return [ 'bikeride' ];
+    }
+
+
+    /**
+     * Get widget keywords.
+     *
+     * Retrieve the list of keywords the widget belongs to.
+     *
+     * @since 1.0.0
+     * @access public
+     * @return array Widget keywords.
+     */
+    public function get_keywords(){
+        return [ 'info' ];
+    }
+
+
+    /**
+    * Register widget controls.
+    *
+    * Add input fields to allow the user to customize the widget settings.
+    *
+    * @since 1.0.0
+    * @access protected
+    */
+    protected function register_controls(){ 
+
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__( 'Content', 'essential-elementor-widget' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+            $this->add_control(
+                'br_info_title',
+                [
+                    'label' => esc_html__( 'BR Info title', 'bikeride' ),
+                    'type' => \Elementor\Controls_Manager::TEXT,
+                    'label_block' => true,
+                    'placeholder' => esc_html__( 'Your BR Info title here', 'bikeride' ),
+                ]
+            );
+
+            $this->add_control(
+                'br_info_description',
+                [
+                    'label' => esc_html__( 'BR Info Description', 'bikeride' ),
+                    'type' => \Elementor\Controls_Manager::TEXTAREA,
+                    'rows' => 10,
+                    'default' => '',
+                    'placeholder' => esc_html__( 'Your BR Description here', 'bikeride' ),
+                ]
+            );
+
+		$this->end_controls_section();
+
+    }
+
+
+    /**
+    * Render widget output on the frontend.
+    *
+    * Written in PHP and used to generate the final HTML.
+    *
+    * @since 1.0.0
+    * @access protected
+    */
+    protected function render(){
+
+        // get our input from the widget settings.
+        $settings = $this->get_settings_for_display();
+
+        // get the individual values of the input
+        $br_info_title = $settings['br_info_title'];
+        $br_info_description = $settings['br_info_description'];
+
+        ?>
+
+        <!-- Start rendering the output -->        
+        <div class="elem-info-space">
+            <div class="info-space-square"></div>
+            <div class="info-space-data">
+                <div class="info-space-title">
+                    <?php echo $br_info_title; ?>
+                </div>
+                <div class="info-space-description">
+                <?php echo $br_info_description; ?>
+                </div>
+            </div>
+        </div>        
+        <!-- End rendering the output -->
+
+        <?php
+
+    }
+
+}
