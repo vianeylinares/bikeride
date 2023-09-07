a96d65681d1dfb2a6cc825ca8040c22ef010f4c1
vianey <vianeylinares@gmail.com>
2023-09-06 18:57:27 -0700

    Enqueued font-awesome font as css file. Also, changed font-awesome display at footer.

--- bikeride/footer.php
+++ bikeride/footer.php
@@ -10,10 +10,10 @@
             </main>
             <footer class="footer">
                 <div class="social">
-                    <a href="<?php echo esc_url( get_theme_mod( 'set_facebook' ) ); ?>" target="_blank"><i class="fa fa-facebook-square"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_facebook' ) ); ?>" target="_blank"><i class="fa fa-facebook"></i></a>
                     <a href="<?php echo esc_url( get_theme_mod( 'set_instagram' ) ); ?>" target="_blank"><i class="fa fa-instagram"></i></a>
-                    <a href="<?php echo esc_url( get_theme_mod( 'set_pinterest' ) ); ?>" target="_blank"><i class="fa fa-pinterest-square"></i></a>
-                    <a href="<?php echo esc_url( get_theme_mod( 'set_youtube' ) ); ?>" target="_blank"><i class="fa fa-youtube-square"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_pinterest' ) ); ?>" target="_blank"><i class="fa fa-pinterest"></i></a>
+                    <a href="<?php echo esc_url( get_theme_mod( 'set_youtube' ) ); ?>" target="_blank"><i class="fa fa-youtube"></i></a>
                 </div>
                 <div class="copyright-policies">
                 <?php echo esc_html( get_theme_mod( 'set_copyright', __( 'Copyright - All Rights Reserved', 'bikeride' ) ) ); ?>
--- bikeride/functions.php
+++ bikeride/functions.php
@@ -22,7 +22,7 @@ require_once get_template_directory() . '/elementor/register.php';
 
 function bikeride_scripts(){
 
-    wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css', 'all' );
+    wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome.css', 'all' );
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Pacifico&display=swap', array(), null );
     wp_enqueue_style( 'bikeride-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
 
