85e26227cb2421d57dcf50010596468811f91beb
vianey <vianeylinares@gmail.com>
2023-10-17 18:54:34 -0700

    Added theme support for WooCommerce.

--- bikeride/functions.php
+++ bikeride/functions.php
@@ -64,6 +64,8 @@ function bikeride_config(){
 
     add_theme_support( 'post-thumbnails' );
 
+    add_theme_support( 'woocommerce' );
+
 }
 add_action( 'after_setup_theme', 'bikeride_config', 0 );
 
